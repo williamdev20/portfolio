459960e9f272cf9b7c329ccfcc3837d543f5a026
williamdev20 <williamalvs45@gmail.com>
2026-02-17 19:33:04 -0300

    refactor(frontend): remover variáveis de simulação do ambiente de desenvolvimento

--- frontend/src/components/Certification.jsx
+++ frontend/src/components/Certification.jsx
@@ -3,45 +3,6 @@ import api from "../api/api";
 import CredentialCard from "./cards/CredentialCard";
 import arrow from "../assets/arrows/arrow.png";
 
-const certificados = [
-    {
-        img: "https://portoalegre.wordcamp.org/2017/files/2017/03/senac-logo.png?w=640",
-        title: "Análise e Desenvolvimento de Sistemas",
-        desc: "Iniciado em Fev. de 2025 (Em andamento)",
-        credential: "Ver credencial"
-    },
-    {
-        img: "https://cdn.freebiesupply.com/images/thumbs/2x/cisco-logo.png",
-        title: "Fundamentos do Python 1",
-        desc: "Concluído em Jan. de 2026",
-        credential: "Ver credencial"
-    },
-    {
-        img: "https://cdn.freebiesupply.com/images/thumbs/2x/cisco-logo.png",
-        title: "Defesa de Rede",
-        desc: "Concluído em Mar. de 2025",
-        credential: "Ver credencial"
-    },
-    {
-        img: "https://cdn.freebiesupply.com/images/thumbs/2x/cisco-logo.png",
-        title: "Gerenciamento de Ameaças Cibernéticas",
-        desc: "Concluído em Sep. de 2025",
-        credential: "Ver credencial"
-    },
-    {
-        img: "https://portoalegre.wordcamp.org/2017/files/2017/03/senac-logo.png?w=640",
-        title: "Análise e Desenvolvimento de Sistemas",
-        desc: "Iniciado em Fev. de 2025 (Em andamento)",
-        credential: "Ver credencial"
-    },
-    {
-        img: "https://portoalegre.wordcamp.org/2017/files/2017/03/senac-logo.png?w=640",
-        title: "Análise e Desenvolvimento de Sistemas",
-        desc: "Iniciado em Fev. de 2025 (Em andamento)",
-        credential: "Ver credencial"
-    },
-]
-
 export default function Certification() {
     const [certifications, setCertifications] = useState([]);
 
@@ -99,7 +60,7 @@ export default function Certification() {
 
             <div className="flex flex-row gap-5 items-center mb-auto">
                 <button className="cursor-pointer md:m-2" type="button" onClick={previous}>
-                    <img className="w-10 h-10 bg-white hover:bg-gray-100 rounded-4xl" src={arrow} alt="arrow" />
+                    <img className="w-8 h-8 md:w-10 md:h-10 bg-white hover:bg-gray-100 rounded-4xl" src={arrow} alt="arrow" />
                 </button>
 
                 <div className="overflow-hidden py-2" style={{ width: `${visibles * cardWidth + (visibles - 1) * gap}px` }}>
@@ -122,7 +83,7 @@ export default function Certification() {
                 </div>
 
                 <button className="cursor-pointer md:m-2" type="button" onClick={next}>
-                    <img className="w-10 h-10 rotate-180 bg-white hover:bg-gray-100 rounded-4xl" src={arrow} alt="arrow" />
+                    <img className="w-8 h-8 md:w-10 md:h-10 rotate-180 bg-white hover:bg-gray-100 rounded-4xl" src={arrow} alt="arrow" />
                 </button>
             </div>
         </section>
--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react"
 import api from "../api/api"
 import Card from "./cards/Card"
-import iara_firmware from "../assets/projects/iara-firmware.jpg"
-import iara_web from "../assets/projects/iara-web.jpg"
-import tomas from "../assets/projects/tomas.jpg"
-import flashcard from "../assets/projects/flashcard.jpg"
-import portfolio from "../assets/background/background.svg"
 
 export default function Projects() {
     const [projects, setProjects] = useState([]);
@@ -35,49 +30,6 @@ export default function Projects() {
                         />
                     ))
                 }
-                
-                {/*
-                <Card
-                    img={iara_firmware}
-                    title={"IARA | Firmware"}
-                    stack={["C++", "C"]}
-                    desc={"Protótipo do firmware de um projeto colaborativo de um colar inteligente para capturar sinais em Libras e enviá-los a um modelo de visão computacional identifica os gestos e os converte em fala em tempo real."}
-                    github={"https://github.com/williamdev20/iara"}
-                    link={"https://tomasdev-test.vercel.app/"}
-                />
-                <Card
-                    img={iara_web}
-                    title={"IARA | Web"}
-                    stack={["Python", "Django"]}
-                    desc={"Versão Web de um projeto colaborativo de um colar inteligente para capturar sinais em Libras e enviá-los a um modelo de visão computacional identifica os gestos e os converte em fala em tempo real."}
-                    github={"https://github.com/williamdev20/iara-web"}
-                    link={"https://tomasdev-test.vercel.app/"}
-                /> 
-                <Card
-                    img={tomas}
-                    title={"TOMAS | Web"}
-                    stack={["JS", "Express", "PostgreSQL", "React"]}
-                    desc={"Projeto Integrador desenvolvido por alunos do 1º ano C do Senac Paulista (PE). O projeto é uma RAG projetada para atender às necessidades de pessoas neurodivergentes."}
-                    github={"https://github.com/harisoncleytondev/tomas"}
-                    link={"https://tomasdev-test.vercel.app/"}
-                /> 
-                <Card
-                    img={portfolio}
-                    title={"Portfolio"}
-                    stack={["Python", "DRF", "React", "Tailwindcss"]}
-                    desc={"Portfolio Web com sistema de CMS e backoffice com o painel admin do Django."}
-                    github={"https://github.com/williamdev20/portfolio"}
-                    link={"https://tomasdev-test.vercel.app/"}
-                />
-                <Card
-                    img={flashcard}
-                    title={"Flashcard AI"}
-                    stack={["Python", "DRF", "Docker", "PostgreSQL"]}
-                    desc={"O Flashcard AI é um projeto colaborativo de criação de flashcards inteligentes de uso manual ou com Inteligência Artifical para ajudar a vida dos estudantes."}
-                    github={"https://github.com/Flashcard-AI/flashcard-ai"}
-                    link={"https://tomasdev-test.vercel.app/"}
-                />
-                */}
             </div>
         </section>
     )
